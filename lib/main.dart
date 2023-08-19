50af5fc173b29d93b9b771dd5e856ca283bcda1d
krishnas888 <verakrishnas@gmail.com>
2023-08-19 18:24:50 +0530

    RE FACTORING CODE
    components that improve the readability and maintainability of our code

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,32 @@
-import 'dart:ui';
+import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
-import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'components/player.dart';
 
 void main() async {
+  // Create an instance of the game
   final goldRush = GoldRush();
+
+  // Setup Flutter widgets and start the game in full screen portrait orientation
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
   await Flame.device.setPortrait();
+
+  // Every is a widget in Flutter! So run the app passing the games widget here
   runApp(
        GameWidget(game: goldRush)
   );
 }
 
-class GoldRush with Loadable, Game{
-
-  static const int squareSpeed = 250;
-  static final squarePaint = BasicPalette.green.paint();
-  static final squareWidth = 100.0,
-  squareHeight = 100.0;
-  late Rect squarePos;
-  int squareDirection = 1;
-  late double screenWidth, screenHeight, centerX, centerY;
-
-  @override
+class GoldRush extends FlameGame with HasCollidables{
+@override
   Future<void> onLoad() async{
-    super.onLoad();
-    screenWidth = MediaQueryData.fromWindow(window).size.width;
-    screenHeight = MediaQueryData.fromWindow(window).size.height;
-    centerX = (screenWidth / 2) -(squareWidth / 2);
-    centerY = (screenHeight / 2) - (screenHeight / 2);
-    squarePos = Rect.fromLTWH(centerX, centerY, squareWidth, screenHeight);
-  }
+  super.onLoad();
+  add(Player());
 
-  @override
-  void render(Canvas canvas) {
-    canvas.drawRect(squarePos, squarePaint);
-  }
+  add(Player());
+  add(ScreenCollidable());
+}
 
-  @override
-  void update(double deltaTime) {
-    squarePos =squarePos.translate(squareSpeed * squareDirection * deltaTime, 0);
-    if(squareDirection == -1 && squarePos.left > 0){
-      squareDirection == -1;
-    }else if(squareDirection == -1 && squarePos.left <0){
-      squareDirection = 1;
-    }
-  }
 }
